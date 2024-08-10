56b320c4c91f2863302e48e71254589e9652cb65
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-08-11 00:06:43 +0530

    ditto

--- api/api.go
+++ api/api.go
@@ -145,59 +145,7 @@ func commandMapbHelper(p *LocationLog) {
     return
 }
 
-// Struct to store incomming encounters data
-type explore struct {
-    EncounterMethodRates []struct {
-        EncounterMethod struct {
-            Name string `json:"name"`
-            URL  string `json:"url"`
-        } `json:"encounter_method"`
-        VersionDetails []struct {
-            Rate    int `json:"rate"`
-            Version struct {
-                Name string `json:"name"`
-                URL  string `json:"url"`
-            } `json:"version"`
-        } `json:"version_details"`
-    } `json:"encounter_method_rates"`
-    GameIndex int `json:"game_index"`
-    ID        int `json:"id"`
-    Location  struct {
-        Name string `json:"name"`
-        URL  string `json:"url"`
-    } `json:"location"`
-    Name  string `json:"name"`
-    Names []struct {
-        Language struct {
-            Name string `json:"name"`
-            URL  string `json:"url"`
-        } `json:"language"`
-        Name string `json:"name"`
-    } `json:"names"`
-    PokemonEncounters []struct {
-        Pokemon struct {
-            Name string `json:"name"`
-            URL  string `json:"url"`
-        } `json:"pokemon"`
-        VersionDetails []struct {
-            EncounterDetails []struct {
-                Chance          int           `json:"chance"`
-                ConditionValues []interface{} `json:"condition_values"`
-                MaxLevel        int           `json:"max_level"`
-                Method          struct {
-                    Name string `json:"name"`
-                    URL  string `json:"url"`
-                } `json:"method"`
-                MinLevel int `json:"min_level"`
-            } `json:"encounter_details"`
-            MaxChance int `json:"max_chance"`
-            Version   struct {
-                Name string `json:"name"`
-                URL  string `json:"url"`
-            } `json:"version"`
-        } `json:"version_details"`
-    } `json:"pokemon_encounters"`
-}
+
 
 // Explore function all the pokemon present in a given area
 func Explore(areas ...string) error {
